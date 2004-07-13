c52201a823f880284027a1e2ccf33cdc16e8dc50
pferraro <ferraro@users.sourceforge.net>
2004-07-13 07:29:49 +0000

    1st cut
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@86 882a69a1-a10d-0410-8702-ae3607fe02de

--- ha-jdbc/src/net/sf/ha/jdbc/DistributedClusterManager.java
+++ ha-jdbc/src/net/sf/ha/jdbc/DistributedClusterManager.java
@@ -1,11 +1,78 @@
 package net.sf.ha.jdbc;
 
+import java.io.Serializable;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jgroups.Address;
+import org.jgroups.blocks.NotificationBus;
+
 /**
  * @author  Paul Ferraro
  * @version $Revision$
  * @since   1.0
  */
-public class DistributedClusterManager extends DatabaseClusterManager implements DistributedClusterManagerMBean
+public class DistributedClusterManager extends DatabaseClusterManager implements DistributedClusterManagerMBean, NotificationBus.Consumer
 {
+	private static Log log = LogFactory.getLog(DistributedClusterManager.class);
+	
+	private NotificationBus notificationBus;
+	
+	public DistributedClusterManager()
+	{
+		try
+		{
+			this.notificationBus = new NotificationBus("");
+			this.notificationBus.start();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	protected void finalize()
+	{
+		this.notificationBus.stop();
+	}
+	
+	public void deactivate(DatabaseCluster cluster, Database database)
+	{
+		this.notificationBus.sendNotification(new String[] { cluster.getDescriptor().getName(), database.getId() });
+	}
 
+	/**
+	 * @see org.jgroups.blocks.NotificationBus.Consumer#handleNotification(java.io.Serializable)
+	 */
+	public void handleNotification(Serializable object)
+	{
+		String[] notification = (String[]) object;
+		String clusterName = notification[0];
+		String databaseId = notification[1];
+		
+	}
+
+	/**
+	 * @see org.jgroups.blocks.NotificationBus.Consumer#getCache()
+	 */
+	public Serializable getCache()
+	{
+		return null;
+	}
+
+	/**
+	 * @see org.jgroups.blocks.NotificationBus.Consumer#memberJoined(org.jgroups.Address)
+	 */
+	public void memberJoined(Address address)
+	{
+		log.info("Member joined: " + address);
+	}
+
+	/**
+	 * @see org.jgroups.blocks.NotificationBus.Consumer#memberLeft(org.jgroups.Address)
+	 */
+	public void memberLeft(Address address)
+	{
+		log.info("Member left: " + address);
+	}
 }
