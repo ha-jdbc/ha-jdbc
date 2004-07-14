093ff24edc9b38d56288c97c8c018e418f815f01
pferraro <ferraro@users.sourceforge.net>
2004-07-14 04:27:21 +0000

    Removed obsolete references to DatabaseCluster
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@104 882a69a1-a10d-0410-8702-ae3607fe02de

--- ha-jdbc/src/net/sf/ha/jdbc/pool/xa/XADataSourceProxy.java
+++ ha-jdbc/src/net/sf/ha/jdbc/pool/xa/XADataSourceProxy.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import javax.sql.XAConnection;
 import javax.sql.XADataSource;
 
-import net.sf.ha.jdbc.DatabaseCluster;
 import net.sf.ha.jdbc.pool.ConnectionPoolDataSourceProxy;
 
 /**
@@ -14,18 +13,6 @@ import net.sf.ha.jdbc.pool.ConnectionPoolDataSourceProxy;
  */
 public class XADataSourceProxy extends ConnectionPoolDataSourceProxy implements XADataSource
 {
-	private DatabaseCluster databaseCluster;
-
-	public DatabaseCluster getDatabaseCluster()
-	{
-		return this.databaseCluster;
-	}
-	
-	public void setDatabaseCluster(DatabaseCluster databaseCluster)
-	{
-		this.databaseCluster = databaseCluster;
-	}
-
 	/**
 	 * @see javax.sql.XADataSource#getXAConnection()
 	 */
