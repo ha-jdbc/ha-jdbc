e2c15a183180bc43d634a578f67dfea2a665c64f
pferraro <ferraro@users.sourceforge.net>
2004-08-28 03:02:32 +0000

    Added LGPL license
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@176 882a69a1-a10d-0410-8702-ae3607fe02de

--- ha-jdbc/src/net/sf/ha/jdbc/ResultSetProxy.java
+++ ha-jdbc/src/net/sf/ha/jdbc/ResultSetProxy.java
@@ -1,3 +1,23 @@
+/*
+ * HA-JDBC: High-Availability JDBC
+ * Copyright (C) 2004 Paul Ferraro
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by the 
+ * Free Software Foundation; either version 2.1 of the License, or (at your 
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Contact: ferraro@users.sourceforge.net
+ */
 package net.sf.ha.jdbc;
 
 import java.io.InputStream;
@@ -20,8 +40,9 @@ import java.util.Calendar;
 import java.util.Map;
 
 /**
- * @author Paul Ferraro
+ * @author  Paul Ferraro
  * @version $Revision$
+ * @since   1.0
  */
 public class ResultSetProxy extends SQLProxy implements ResultSet
 {
