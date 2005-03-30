c4a3029b0fec0b903d053922a2b235542b4c6842
pferraro <ferraro@users.sourceforge.net>
2005-03-30 06:08:25 +0000

    Refactored from SQLProxy
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@447 882a69a1-a10d-0410-8702-ae3607fe02de

---
+++ ha-jdbc/src/net/sf/hajdbc/OperationExecutor.java
@@ -0,0 +1,84 @@
+/*
+ * HA-JDBC: High-Availability JDBC
+ * Copyright (C) 2004 Paul Ferraro
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by the 
+ * Free Software Foundation; either version 2.1 of the License, or (at your 
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Contact: ferraro@users.sourceforge.net
+ */
+package net.sf.hajdbc;
+
+import java.util.Map;
+
+/**
+ * Helper class that enables asynchronous execution of an operation.
+ */
+public class OperationExecutor implements Runnable
+{
+	private DatabaseCluster databaseCluster;
+	private Operation operation;
+	private Database database;
+	private Object object;
+	private Map returnValueMap;
+	private Map exceptionMap;
+	
+	/**
+	 * Constructs a new OperationExecutor.
+	 * @param operation a database operation
+	 * @param database a database descriptor
+	 * @param sqlObject a SQL object
+	 * @param returnValueMap a Map<Database, Object> that holds the results from the operation execution
+	 * @param exceptionMap a Map<Database, SQLException> that holds the exceptions resulting from the operation execution
+	 */
+	public OperationExecutor(DatabaseCluster databaseCluster, Operation operation, Database database, Object object, Map returnValueMap, Map exceptionMap)
+	{
+		this.databaseCluster = databaseCluster;
+		this.operation = operation;
+		this.database = database;
+		this.object = object;
+		this.returnValueMap = returnValueMap;
+		this.exceptionMap = exceptionMap;
+	}
+	
+	/**
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run()
+	{
+		try
+		{
+			Object returnValue = this.operation.execute(this.database, this.object);
+			
+			synchronized (this.returnValueMap)
+			{
+				this.returnValueMap.put(this.database, returnValue);
+			}
+		}
+		catch (Throwable e)
+		{
+			try
+			{
+				this.databaseCluster.handleFailure(database, e);
+			}
+			catch (Throwable exception)
+			{
+				synchronized (this.exceptionMap)
+				{
+					this.exceptionMap.put(this.database, e);
+				}
+			}
+		}
+	}
+}
