ce2a6f1eafbde2dcd822d69e7d4a3d85be1dbc95
pferraro <ferraro@users.sourceforge.net>
2005-04-18 01:20:23 +0000

    Fixed ClassCastException in rollback(Savepoint) and releaseSavepoint(...) methods
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@508 882a69a1-a10d-0410-8702-ae3607fe02de

--- ha-jdbc/src/net/sf/hajdbc/sql/Connection.java
+++ ha-jdbc/src/net/sf/hajdbc/sql/Connection.java
@@ -48,38 +48,6 @@ public class Connection extends SQLObject implements java.sql.Connection
 	}
 	
 	/**
-	 * @see java.sql.Connection#getHoldability()
-	 */
-	public int getHoldability() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return new Integer(connection.getHoldability());
-			}
-		};
-		
-		return ((Integer) this.executeReadFromDriver(operation)).intValue();
-	}
-
-	/**
-	 * @see java.sql.Connection#getTransactionIsolation()
-	 */
-	public int getTransactionIsolation() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return new Integer(connection.getTransactionIsolation());
-			}
-		};
-		
-		return ((Integer) this.executeReadFromDatabase(operation)).intValue();
-	}
-
-	/**
 	 * @see java.sql.Connection#clearWarnings()
 	 */
 	public void clearWarnings() throws SQLException
@@ -125,276 +93,6 @@ public class Connection extends SQLObject implements java.sql.Connection
 			public Object execute(Database database, java.sql.Connection connection) throws SQLException
 			{
 				connection.commit();
-				
-				return null;
-			}
-		};
-		
-		this.executeWriteToDatabase(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#rollback()
-	 */
-	public void rollback() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				connection.rollback();
-				
-				return null;
-			}
-		};
-		
-		this.executeWriteToDatabase(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#getAutoCommit()
-	 */
-	public boolean getAutoCommit() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return new Boolean(connection.getAutoCommit());
-			}
-		};
-		
-		return ((Boolean) this.executeReadFromDriver(operation)).booleanValue();
-	}
-
-	/**
-	 * @see java.sql.Connection#isClosed()
-	 */
-	public boolean isClosed() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return new Boolean(connection.isClosed());
-			}
-		};
-		
-		return ((Boolean) this.executeReadFromDriver(operation)).booleanValue();
-	}
-
-	/**
-	 * @see java.sql.Connection#isReadOnly()
-	 */
-	public boolean isReadOnly() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return new Boolean(connection.isReadOnly());
-			}
-		};
-		
-		return ((Boolean) this.executeReadFromDriver(operation)).booleanValue();
-	}
-
-	/**
-	 * @see java.sql.Connection#setHoldability(int)
-	 */
-	public void setHoldability(final int holdability) throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				connection.setHoldability(holdability);
-				
-				return null;
-			}
-		};
-		
-		this.executeWriteToDriver(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#setTransactionIsolation(int)
-	 */
-	public void setTransactionIsolation(final int transactionIsolation) throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				connection.setTransactionIsolation(transactionIsolation);
-				
-				return null;
-			}
-		};
-		
-		this.executeWriteToDatabase(operation);
-		
-		this.record(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#setAutoCommit(boolean)
-	 */
-	public void setAutoCommit(final boolean autoCommit) throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				connection.setAutoCommit(autoCommit);
-				
-				return null;
-			}
-		};
-		
-		this.executeWriteToDatabase(operation);
-
-		this.record(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#setReadOnly(boolean)
-	 */
-	public void setReadOnly(final boolean readOnly) throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				connection.setReadOnly(readOnly);
-				
-				return null;
-			}
-		};
-		
-		this.executeWriteToDriver(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#getCatalog()
-	 */
-	public String getCatalog() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return connection.getCatalog();
-			}
-		};
-		
-		return (String) this.executeReadFromDatabase(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#setCatalog(java.lang.String)
-	 */
-	public void setCatalog(final String catalog) throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				connection.setCatalog(catalog);
-				
-				return null;
-			}
-		};
-		
-		this.executeWriteToDatabase(operation);
-		
-		this.record(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#getMetaData()
-	 */
-	public DatabaseMetaData getMetaData() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return connection.getMetaData();
-			}
-		};
-		
-		return (DatabaseMetaData) this.executeReadFromDatabase(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#getWarnings()
-	 */
-	public SQLWarning getWarnings() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return connection.getWarnings();
-			}
-		};
-		
-		return (SQLWarning) this.executeReadFromDriver(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#setSavepoint()
-	 */
-	public java.sql.Savepoint setSavepoint() throws SQLException
-	{
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				return connection.setSavepoint();
-			}
-		};
-		
-		return new Savepoint(this, operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#releaseSavepoint(java.sql.Savepoint)
-	 */
-	public void releaseSavepoint(java.sql.Savepoint savepoint) throws SQLException
-	{
-		final Savepoint savepointProxy = (Savepoint) savepoint;
-		
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				Savepoint savepoint = (Savepoint) savepointProxy.getObject(database);
-				
-				connection.releaseSavepoint(savepoint);
-				
-				return null;
-			}
-		};
-		
-		this.executeWriteToDatabase(operation);
-	}
-
-	/**
-	 * @see java.sql.Connection#rollback(java.sql.Savepoint)
-	 */
-	public void rollback(java.sql.Savepoint savepoint) throws SQLException
-	{
-		final Savepoint proxy = (Savepoint) savepoint;
-		
-		ConnectionOperation operation = new ConnectionOperation()
-		{
-			public Object execute(Database database, java.sql.Connection connection) throws SQLException
-			{
-				Savepoint savepoint = (Savepoint) proxy.getObject(database);
-				
-				connection.rollback(savepoint);
 				
 				return null;
 			}
@@ -452,6 +150,86 @@ public class Connection extends SQLObject implements java.sql.Connection
 	}
 
 	/**
+	 * @see java.sql.Connection#getAutoCommit()
+	 */
+	public boolean getAutoCommit() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				return new Boolean(connection.getAutoCommit());
+			}
+		};
+		
+		return ((Boolean) this.executeReadFromDriver(operation)).booleanValue();
+	}
+
+	/**
+	 * @see java.sql.Connection#getCatalog()
+	 */
+	public String getCatalog() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				return connection.getCatalog();
+			}
+		};
+		
+		return (String) this.executeReadFromDatabase(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#getHoldability()
+	 */
+	public int getHoldability() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				return new Integer(connection.getHoldability());
+			}
+		};
+		
+		return ((Integer) this.executeReadFromDriver(operation)).intValue();
+	}
+
+	/**
+	 * @see java.sql.Connection#getMetaData()
+	 */
+	public DatabaseMetaData getMetaData() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				return connection.getMetaData();
+			}
+		};
+		
+		return (DatabaseMetaData) this.executeReadFromDatabase(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#getTransactionIsolation()
+	 */
+	public int getTransactionIsolation() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				return new Integer(connection.getTransactionIsolation());
+			}
+		};
+		
+		return ((Integer) this.executeReadFromDatabase(operation)).intValue();
+	}
+
+	/**
 	 * @see java.sql.Connection#getTypeMap()
 	 */
 	public Map getTypeMap() throws SQLException
@@ -468,21 +246,51 @@ public class Connection extends SQLObject implements java.sql.Connection
 	}
 
 	/**
-	 * @see java.sql.Connection#setTypeMap(java.util.Map)
+	 * @see java.sql.Connection#getWarnings()
 	 */
-	public void setTypeMap(final Map map) throws SQLException
+	public SQLWarning getWarnings() throws SQLException
 	{
 		ConnectionOperation operation = new ConnectionOperation()
 		{
 			public Object execute(Database database, java.sql.Connection connection) throws SQLException
 			{
-				connection.setTypeMap(map);
-				
-				return null;
+				return connection.getWarnings();
 			}
 		};
 		
-		this.executeWriteToDriver(operation);
+		return (SQLWarning) this.executeReadFromDriver(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#isClosed()
+	 */
+	public boolean isClosed() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				return new Boolean(connection.isClosed());
+			}
+		};
+		
+		return ((Boolean) this.executeReadFromDriver(operation)).booleanValue();
+	}
+
+	/**
+	 * @see java.sql.Connection#isReadOnly()
+	 */
+	public boolean isReadOnly() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				return new Boolean(connection.isReadOnly());
+			}
+		};
+		
+		return ((Boolean) this.executeReadFromDriver(operation)).booleanValue();
 	}
 
 	/**
@@ -646,6 +454,160 @@ public class Connection extends SQLObject implements java.sql.Connection
 	}
 
 	/**
+	 * @see java.sql.Connection#releaseSavepoint(java.sql.Savepoint)
+	 */
+	public void releaseSavepoint(java.sql.Savepoint savepoint) throws SQLException
+	{
+		final Savepoint savepointProxy = (Savepoint) savepoint;
+		
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				java.sql.Savepoint savepoint = (java.sql.Savepoint) savepointProxy.getObject(database);
+				
+				connection.releaseSavepoint(savepoint);
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDatabase(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#rollback()
+	 */
+	public void rollback() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				connection.rollback();
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDatabase(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#rollback(java.sql.Savepoint)
+	 */
+	public void rollback(java.sql.Savepoint savepoint) throws SQLException
+	{
+		final Savepoint proxy = (Savepoint) savepoint;
+		
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				java.sql.Savepoint savepoint = (java.sql.Savepoint) proxy.getObject(database);
+				
+				connection.rollback(savepoint);
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDatabase(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#setAutoCommit(boolean)
+	 */
+	public void setAutoCommit(final boolean autoCommit) throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				connection.setAutoCommit(autoCommit);
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDatabase(operation);
+
+		this.record(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#setCatalog(java.lang.String)
+	 */
+	public void setCatalog(final String catalog) throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				connection.setCatalog(catalog);
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDatabase(operation);
+		
+		this.record(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#setHoldability(int)
+	 */
+	public void setHoldability(final int holdability) throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				connection.setHoldability(holdability);
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDriver(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#setReadOnly(boolean)
+	 */
+	public void setReadOnly(final boolean readOnly) throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				connection.setReadOnly(readOnly);
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDriver(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#setSavepoint()
+	 */
+	public java.sql.Savepoint setSavepoint() throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				return connection.setSavepoint();
+			}
+		};
+		
+		return new Savepoint(this, operation);
+	}
+
+	/**
 	 * @see java.sql.Connection#setSavepoint(java.lang.String)
 	 */
 	public java.sql.Savepoint setSavepoint(final String name) throws SQLException
@@ -659,5 +621,43 @@ public class Connection extends SQLObject implements java.sql.Connection
 		};
 		
 		return new Savepoint(this, operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#setTransactionIsolation(int)
+	 */
+	public void setTransactionIsolation(final int transactionIsolation) throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				connection.setTransactionIsolation(transactionIsolation);
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDatabase(operation);
+		
+		this.record(operation);
+	}
+
+	/**
+	 * @see java.sql.Connection#setTypeMap(java.util.Map)
+	 */
+	public void setTypeMap(final Map map) throws SQLException
+	{
+		ConnectionOperation operation = new ConnectionOperation()
+		{
+			public Object execute(Database database, java.sql.Connection connection) throws SQLException
+			{
+				connection.setTypeMap(map);
+				
+				return null;
+			}
+		};
+		
+		this.executeWriteToDriver(operation);
 	}
 }
