29a5a51e1be99f214caa0a5285c19148eb41f5eb
pferraro <ferraro@users.sourceforge.net>
2005-07-14 08:35:31 +0000

    Created
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@582 882a69a1-a10d-0410-8702-ae3607fe02de

---
+++ ha-jdbc/test/net/sf/hajdbc/sync/TestForeignKey.java
@@ -0,0 +1,155 @@
+/*
+ * HA-JDBC: High-Availability JDBC
+ * Copyright (C) 2004 Paul Ferraro
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by the 
+ * Free Software Foundation; either version 2.1 of the License, or (at your 
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Contact: ferraro@users.sourceforge.net
+ */
+package net.sf.hajdbc.sync;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.Collections;
+
+import net.sf.hajdbc.AbstractTestCase;
+
+import org.easymock.MockControl;
+
+public class TestForeignKey extends AbstractTestCase
+{
+	MockControl connectionControl = this.createControl(Connection.class);
+	Connection connection = (Connection) connectionControl.getMock();
+	
+	MockControl metaDataControl = this.createControl(DatabaseMetaData.class);
+	DatabaseMetaData metaData = (DatabaseMetaData) metaDataControl.getMock();
+	
+	MockControl statementControl = this.createControl(Statement.class);
+	Statement statement = (Statement) statementControl.getMock();
+	
+	MockControl resultSetControl = this.createControl(ResultSet.class);
+	ResultSet resultSet = (ResultSet) resultSetControl.getMock();
+	
+	/*
+	 * Test method for 'net.sf.hajdbc.sync.ForeignKey.collect(Connection, List)'
+	 */
+	public void testCollect()
+	{
+		try
+		{
+			this.connection.getMetaData();
+			this.connectionControl.setReturnValue(this.metaData);
+			
+			this.metaData.getIdentifierQuoteString();
+			this.metaDataControl.setReturnValue("'");
+
+			this.metaData.getImportedKeys(null, null, "fk_table");
+			this.metaDataControl.setReturnValue(this.resultSet);
+			
+			this.resultSet.next();
+			this.resultSetControl.setReturnValue(true);
+			
+			this.resultSet.getString("FK_NAME");
+			this.resultSetControl.setReturnValue("fk");
+			
+			this.resultSet.getString("FKCOLUMN_NAME");
+			this.resultSetControl.setReturnValue("fk_col");
+			
+			this.resultSet.getString("PKTABLE_NAME");
+			this.resultSetControl.setReturnValue("pk_table");
+
+			this.resultSet.getString("PKCOLUMN_NAME");
+			this.resultSetControl.setReturnValue("pk_col");
+
+			this.resultSet.next();
+			this.resultSetControl.setReturnValue(false);
+			
+			this.resultSet.close();
+			this.resultSetControl.setVoidCallable();
+			
+			this.replay();
+			
+			Collection collection = ForeignKey.collect(this.connection, Collections.singletonList("fk_table"));
+			
+			this.verify();
+			
+			assertNotNull(collection);
+			assertEquals(1, collection.size());
+			assertEquals(new ForeignKey("fk", "fk_table", "fk_col", "pk_table", "pk_col", "'"), collection.iterator().next());
+		}
+		catch (SQLException e)
+		{
+			this.fail(e);
+		}
+	}
+
+	/*
+	 * Test method for 'net.sf.hajdbc.sync.Key.hashCode()'
+	 */
+	public void testHashCode()
+	{
+		ForeignKey key = new ForeignKey("test", null, null, null, null, null);
+		
+		assertEquals("test".hashCode(), key.hashCode());
+	}
+
+	/*
+	 * Test method for 'net.sf.hajdbc.sync.Key.equals(Object)'
+	 */
+	public void testEqualsObject()
+	{
+		ForeignKey key1 = new ForeignKey("test", "", "", "", "", "");
+		ForeignKey key2 = new ForeignKey("test", null, null, null, null, null);
+		ForeignKey key3 = new ForeignKey("testing", null, null, null, null, null);
+		
+		assertTrue(key1.equals(key2));
+		assertFalse(key1.equals(key3));
+		assertFalse(key2.equals(key3));
+	}
+
+	/*
+	 * Test method for 'net.sf.hajdbc.sync.Key.executeSQL(Connection, Collection, String)'
+	 */
+	public void testExecuteSQL()
+	{
+		try
+		{
+			this.connection.createStatement();
+			this.connectionControl.setReturnValue(this.statement);
+			
+			this.statement.execute("ALTER TABLE fk_table ADD CONSTRAINT 'fk' FOREIGN KEY (fk_col) REFERENCES 'pk_table' (pk_col)");
+			this.statementControl.setReturnValue(true);
+			
+			this.statement.close();
+			this.statementControl.setVoidCallable();
+			
+			this.replay();
+			
+			ForeignKey key = new ForeignKey("fk", "fk_table", "fk_col", "pk_table", "pk_col", "'");
+			
+			ForeignKey.executeSQL(this.connection, Collections.singleton(key), ForeignKey.DEFAULT_CREATE_SQL);
+			
+			this.verify();
+		}
+		catch (SQLException e)
+		{
+			this.fail(e);
+		}
+	}
+}
---
+++ ha-jdbc/test/net/sf/hajdbc/sync/TestUniqueKey.java
@@ -0,0 +1,173 @@
+/*
+ * HA-JDBC: High-Availability JDBC
+ * Copyright (C) 2004 Paul Ferraro
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by the 
+ * Free Software Foundation; either version 2.1 of the License, or (at your 
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Contact: ferraro@users.sourceforge.net
+ */
+package net.sf.hajdbc.sync;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.easymock.MockControl;
+
+import net.sf.hajdbc.AbstractTestCase;
+
+public class TestUniqueKey extends AbstractTestCase
+{
+	MockControl connectionControl = this.createControl(Connection.class);
+	Connection connection = (Connection) connectionControl.getMock();
+	
+	MockControl metaDataControl = this.createControl(DatabaseMetaData.class);
+	DatabaseMetaData metaData = (DatabaseMetaData) metaDataControl.getMock();
+	
+	MockControl statementControl = this.createControl(Statement.class);
+	Statement statement = (Statement) statementControl.getMock();
+	
+	MockControl resultSetControl = this.createControl(ResultSet.class);
+	ResultSet resultSet = (ResultSet) resultSetControl.getMock();
+	
+
+	/*
+	 * Test method for 'net.sf.hajdbc.sync.UniqueKey.collect(Connection, String, String)'
+	 */
+	public void testCollect()
+	{
+		try
+		{
+			this.connection.getMetaData();
+			this.connectionControl.setReturnValue(this.metaData);
+			
+			this.metaData.getIdentifierQuoteString();
+			this.metaDataControl.setReturnValue("'");
+
+			this.metaData.getIndexInfo(null, null, "table", true, false);
+			this.metaDataControl.setReturnValue(this.resultSet);
+			
+			this.resultSet.next();
+			this.resultSetControl.setReturnValue(true);
+			
+			this.resultSet.getString("INDEX_NAME");
+			this.resultSetControl.setReturnValue("pk");
+			
+			this.resultSet.next();
+			this.resultSetControl.setReturnValue(true);
+
+			this.resultSet.getString("INDEX_NAME");
+			this.resultSetControl.setReturnValue("idx");
+			
+			this.resultSet.getShort("ORDINAL_POSITION");
+			this.resultSetControl.setReturnValue(1);
+			
+			this.resultSet.getString("COLUMN_NAME");
+			this.resultSetControl.setReturnValue("col1");
+			
+			this.resultSet.next();
+			this.resultSetControl.setReturnValue(true);
+
+			this.resultSet.getString("INDEX_NAME");
+			this.resultSetControl.setReturnValue("idx");
+			
+			this.resultSet.getShort("ORDINAL_POSITION");
+			this.resultSetControl.setReturnValue(2);
+			
+			this.resultSet.getString("COLUMN_NAME");
+			this.resultSetControl.setReturnValue("col2");
+
+			this.resultSet.next();
+			this.resultSetControl.setReturnValue(false);
+			
+			this.resultSet.close();
+			this.resultSetControl.setVoidCallable();
+			
+			this.replay();
+			
+			Collection collection = UniqueKey.collect(this.connection, "table", "pk");
+			
+			this.verify();
+			
+			assertNotNull(collection);
+			assertEquals(1, collection.size());
+			assertEquals(new UniqueKey("idx", "table", "'"), collection.iterator().next());
+		}
+		catch (SQLException e)
+		{
+			this.fail(e);
+		}
+	}
+
+	/*
+	 * Test method for 'net.sf.hajdbc.sync.Key.hashCode()'
+	 */
+	public void testHashCode()
+	{
+		UniqueKey key = new UniqueKey("test", null, null);
+		
+		assertEquals("test".hashCode(), key.hashCode());
+	}
+
+	/*
+	 * Test method for 'net.sf.hajdbc.sync.Key.equals(Object)'
+	 */
+	public void testEqualsObject()
+	{
+		UniqueKey key1 = new UniqueKey("test", null, null);
+		UniqueKey key2 = new UniqueKey("test", "", "");
+		UniqueKey key3 = new UniqueKey("testing", "", "");
+		
+		assertTrue(key1.equals(key2));
+		assertFalse(key1.equals(key3));
+		assertFalse(key2.equals(key3));
+	}
+
+	/*
+	 * Test method for 'net.sf.hajdbc.sync.Key.executeSQL(Connection, Collection, String)'
+	 */
+	public void testExecuteSQL()
+	{
+		try
+		{
+			this.connection.createStatement();
+			this.connectionControl.setReturnValue(this.statement);
+			
+			this.statement.execute("ALTER TABLE table ADD CONSTRAINT 'idx' UNIQUE (col1, col2)");
+			this.statementControl.setReturnValue(true);
+			
+			this.statement.close();
+			this.statementControl.setVoidCallable();
+			
+			this.replay();
+			
+			UniqueKey key = new UniqueKey("idx", "table", "'");
+			key.addColumn((short) 2, "col2");
+			key.addColumn((short) 1, "col1");
+			
+			UniqueKey.executeSQL(this.connection, Collections.singleton(key), UniqueKey.DEFAULT_CREATE_SQL);
+			
+			this.verify();
+		}
+		catch (SQLException e)
+		{
+			this.fail(e);
+		}
+	}
+}
