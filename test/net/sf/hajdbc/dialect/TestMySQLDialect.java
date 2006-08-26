b601248a5edc9cc5933af50d7b80e27780b8a071
pferraro <ferraro@users.sourceforge.net>
2006-08-26 21:17:46 +0000

    Implemented.
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/branches/1.1@1318 882a69a1-a10d-0410-8702-ae3607fe02de

--- test/net/sf/hajdbc/dialect/TestMySQLDialect.java
+++ test/net/sf/hajdbc/dialect/TestMySQLDialect.java
@@ -20,11 +20,115 @@
  */
 package net.sf.hajdbc.dialect;
 
+import java.sql.SQLException;
+
+import net.sf.hajdbc.Dialect;
+import net.sf.hajdbc.ForeignKeyConstraint;
+import net.sf.hajdbc.UniqueConstraint;
+
+import org.easymock.EasyMock;
+import org.testng.annotations.Test;
+
 /**
  * @author Paul Ferraro
  *
  */
+@Test
 public class TestMySQLDialect extends TestDefaultDialect
 {
+	/**
+	 * @see net.sf.hajdbc.dialect.TestDefaultDialect#createDialect()
+	 */
+	@Override
+	protected Dialect createDialect()
+	{
+		return new MySQLDialect();
+	}
 
+	/**
+	 * Test case for {@link net.sf.hajdbc.Dialect#getDropForeignKeyConstraintSQL(java.sql.DatabaseMetaData, java.lang.String, java.lang.String, java.lang.String)}
+	 */
+	@Override
+	public void testGetDropForeignKeyConstraintSQL()
+	{
+		ForeignKeyConstraint constraint = new ForeignKeyConstraint("fk_name", "schema", "table");
+		String quote = "'";
+		
+		try
+		{
+			EasyMock.expect(this.metaData.supportsSchemasInDataManipulation()).andReturn(true);
+			EasyMock.expect(this.metaData.getIdentifierQuoteString()).andReturn(quote).times(2);
+			
+			this.control.replay();
+			
+			String sql = this.dialect.getDropForeignKeyConstraintSQL(this.metaData, constraint);
+			
+			this.control.verify();
+			
+			assert sql.equals("ALTER TABLE 'schema'.'table' DROP FOREIGN KEY fk_name") : sql;
+		}
+		catch (SQLException e)
+		{
+			assert false : e;
+		}
+	}
+
+	/**
+	 * Test case for {@link net.sf.hajdbc.Dialect#getCreateUniqueConstraintSQL(java.sql.DatabaseMetaData, java.lang.String, java.lang.String, java.lang.String, java.util.List)}
+	 */
+	@Override
+	public void testGetCreateUniqueConstraintSQL()
+	{
+		UniqueConstraint constraint = new UniqueConstraint("uk_name", "schema", "table");
+		constraint.getColumnList().add("column1");
+		constraint.getColumnList().add("column2");
+		String quote = "'";
+		
+		try
+		{
+			EasyMock.expect(this.metaData.getIdentifierQuoteString()).andReturn(quote).times(2);
+			EasyMock.expect(this.metaData.supportsSchemasInDataManipulation()).andReturn(true);
+			EasyMock.expect(this.metaData.getIdentifierQuoteString()).andReturn(quote).times(2);
+			
+			this.control.replay();
+			
+			String sql = this.dialect.getCreateUniqueConstraintSQL(this.metaData, constraint);
+			
+			this.control.verify();
+			
+			assert sql.equals("ALTER TABLE 'schema'.'table' ADD UNIQUE uk_name ('column1','column2')") : sql;
+		}
+		catch (SQLException e)
+		{
+			assert false : e;
+		}
+	}
+
+	/**
+	 * Test case for {@link net.sf.hajdbc.Dialect#getDropUniqueConstraintSQL(java.sql.DatabaseMetaData, java.lang.String, java.lang.String, java.lang.String)}
+	 */
+	@Override
+	public void testGetDropUniqueConstraintSQL()
+	{
+		UniqueConstraint constraint = new UniqueConstraint("uk_name", "schema", "table");
+		String quote = "'";
+		
+		try
+		{
+			EasyMock.expect(this.metaData.supportsSchemasInDataManipulation()).andReturn(true);
+			EasyMock.expect(this.metaData.getIdentifierQuoteString()).andReturn(quote).times(2);
+			
+			this.control.replay();
+			
+			String sql = this.dialect.getDropUniqueConstraintSQL(this.metaData, constraint);
+			
+			this.control.verify();
+			
+			assert sql.equals("ALTER TABLE 'schema'.'table' DROP INDEX uk_name") : sql;
+		}
+		catch (SQLException e)
+		{
+			assert false : e;
+		}
+	}
 }
