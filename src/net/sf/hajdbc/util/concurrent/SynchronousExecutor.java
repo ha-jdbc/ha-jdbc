f0b30171a080d640c5cc766321e5074a0806690b
pferraro <ferraro@users.sourceforge.net>
2007-01-12 19:20:11 +0000

    Extends AbstractExecutorService instead on implementing ExecutorService directly - since Java 1.6 api conflicts with Java 1.5 API
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@1464 882a69a1-a10d-0410-8702-ae3607fe02de

--- src/net/sf/hajdbc/util/concurrent/SynchronousExecutor.java
+++ src/net/sf/hajdbc/util/concurrent/SynchronousExecutor.java
@@ -20,14 +20,9 @@
  */
 package net.sf.hajdbc.util.concurrent;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
+import java.util.concurrent.AbstractExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -35,10 +30,34 @@ import java.util.concurrent.TimeUnit;
  * 
  * @author Paul Ferraro
  */
-public class SynchronousExecutor implements ExecutorService
+public class SynchronousExecutor extends AbstractExecutorService
 {
-	private boolean shutdown = false;
+	private boolean shutdown;
 	
+	/**
+	 * @see java.util.concurrent.ExecutorService#awaitTermination(long, java.util.concurrent.TimeUnit)
+	 */
+	public boolean awaitTermination(long time, TimeUnit unit)
+	{
+		return true;
+	}
+
+	/**
+	 * @see java.util.concurrent.ExecutorService#isShutdown()
+	 */
+	public boolean isShutdown()
+	{
+		return this.shutdown;
+	}
+
+	/**
+	 * @see java.util.concurrent.ExecutorService#isTerminated()
+	 */
+	public boolean isTerminated()
+	{
+		return this.shutdown;
+	}
+
 	/**
 	 * @see java.util.concurrent.ExecutorService#shutdown()
 	 */
@@ -58,196 +77,10 @@ public class SynchronousExecutor implements ExecutorService
 	}
 
 	/**
-	 * @see java.util.concurrent.ExecutorService#isShutdown()
-	 */
-	public boolean isShutdown()
-	{
-		return this.shutdown;
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#isTerminated()
-	 */
-	public boolean isTerminated()
-	{
-		return this.isShutdown();
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#awaitTermination(long, java.util.concurrent.TimeUnit)
-	 */
-	public boolean awaitTermination(long timeout, TimeUnit unit)
-	{
-		return true;
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#submit(java.util.concurrent.Callable)
-	 */
-	public <T> Future<T> submit(Callable<T> task)
-	{
-		return new SynchronousFuture<T>(task);
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#submit(java.lang.Runnable, Object)
-	 */
-	public <T> Future<T> submit(Runnable task, T result)
-	{
-		return new SynchronousFuture<T>(new CallableTask<T>(task, result));
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#submit(java.lang.Runnable)
-	 */
-	public Future<?> submit(Runnable task)
-	{
-		return new SynchronousFuture<Void>(new CallableTask<Void>(task, null));
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#invokeAll(java.util.Collection)
-	 */
-	public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks)
-	{
-		List<Future<T>> futureList = new ArrayList<Future<T>>(tasks.size());
-		
-		for (Callable<T> task: tasks)
-		{
-			futureList.add(new SynchronousFuture<T>(task));
-		}
-		
-		return futureList;
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#invokeAll(java.util.Collection, long, java.util.concurrent.TimeUnit)
-	 */
-	public <T> List<Future<T>> invokeAll(Collection<Callable<T>> tasks, long timeout, TimeUnit unit)
-	{
-		return this.invokeAll(tasks);
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#invokeAny(java.util.Collection)
-	 */
-	public <T> T invokeAny(Collection<Callable<T>> tasks) throws ExecutionException
-	{
-		Throwable exception = null;
-		
-		for (Callable<T> task: tasks)
-		{
-			try
-			{
-				return task.call();
-			}
-			catch (Throwable e)
-			{
-				exception = e;
-			}
-		}
-		
-		throw new ExecutionException(exception);
-	}
-
-	/**
-	 * @see java.util.concurrent.ExecutorService#invokeAny(java.util.Collection, long, java.util.concurrent.TimeUnit)
-	 */
-	public <T> T invokeAny(Collection<Callable<T>> tasks, long timeout, TimeUnit unit) throws ExecutionException
-	{
-		return this.invokeAny(tasks);
-	}
-
-	/**
 	 * @see java.util.concurrent.Executor#execute(java.lang.Runnable)
 	 */
 	public void execute(Runnable task)
 	{
 		task.run();
-	}
-	
-	private class SynchronousFuture<T> implements Future<T>
-	{
-		private T result = null;
-		private Throwable exception = null;
-
-		public SynchronousFuture(Callable<T> task)
-		{
-			try
-			{
-				this.result = task.call();
-			}
-			catch (Exception e)
-			{
-				this.exception = e;
-			}
-		}
-		
-		/**
-		 * @see java.util.concurrent.Future#cancel(boolean)
-		 */
-		public boolean cancel(boolean mayInterruptIfRunning)
-		{
-			return false;
-		}
-
-		/**
-		 * @see java.util.concurrent.Future#isCancelled()
-		 */
-		public boolean isCancelled()
-		{
-			return false;
-		}
-
-		/**
-		 * @see java.util.concurrent.Future#isDone()
-		 */
-		public boolean isDone()
-		{
-			return true;
-		}
-
-		/**
-		 * @see java.util.concurrent.Future#get()
-		 */
-		public T get() throws ExecutionException
-		{
-			if (this.exception != null)
-			{
-				throw new ExecutionException(this.exception);
-			}
-			
-			return this.result;
-		}
-
-		/**
-		 * @see java.util.concurrent.Future#get(long, java.util.concurrent.TimeUnit)
-		 */
-		public T get(long timeout, TimeUnit unit) throws ExecutionException
-		{
-			return this.get();
-		}			
-	}
-	
-	private class CallableTask<T> implements Callable<T>
-	{
-		private Runnable task;
-		private T result;
-		
-		public CallableTask(Runnable task, T result)
-		{
-			this.task = task;
-			this.result = result;
-		}
-		
-		/**
-		 * @see java.util.concurrent.Callable#call()
-		 */
-		public T call() throws Exception
-		{
-			this.task.run();
-			
-			return this.result;
-		}		
 	}
 }
