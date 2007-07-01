379c5686528808b59930ae5e8e067772c31ac02d
pferraro <ferraro@users.sourceforge.net>
2007-07-01 05:07:18 +0000

    fixed bad message constant
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@1710 882a69a1-a10d-0410-8702-ae3607fe02de

--- src/net/sf/hajdbc/Messages.java
+++ src/net/sf/hajdbc/Messages.java
@@ -44,7 +44,7 @@ public final class Messages
 	public static final String DATABASE_DEACTIVATED = "database-deactivated";
 	public static final String DATABASE_NOT_ACTIVE = "database-not-active";
 	public static final String DATABASE_NOT_ALIVE = "database-not-alive";
-	public static final String DATABASE_STILL_ACTIVE = "database-remove-failed";
+	public static final String DATABASE_STILL_ACTIVE = "database-still-active";
 	public static final String DATABASE_SYNC_END = "database-sync-end";
 	public static final String DATABASE_SYNC_START = "database-sync-start";
 	public static final String DELETE_COUNT = "delete-count";
