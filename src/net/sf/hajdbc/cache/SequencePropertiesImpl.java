10ef254ac96e3f8bc1890627514e461e609da015
pferraro <ferraro@users.sourceforge.net>
2007-09-10 22:17:01 +0000

    Override equals(...), hashCode(), and toString()
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/branches/2.0@1835 882a69a1-a10d-0410-8702-ae3607fe02de

--- src/net/sf/hajdbc/cache/SequencePropertiesImpl.java
+++ src/net/sf/hajdbc/cache/SequencePropertiesImpl.java
@@ -25,4 +25,35 @@ public class SequencePropertiesImpl implements SequenceProperties
 	{
 		return this.name;
 	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object object)
+	{
+		if ((object == null) || !(object instanceof SequenceProperties)) return false;
+		
+		SequenceProperties sequence = (SequenceProperties) object;
+		
+		return this.name.equals(sequence.getName());
+	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		return this.name.hashCode();
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		return this.name;
+	}
 }
