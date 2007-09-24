2f387be3bc64ec6aa9b5c6f5a96b8db84332ead7
pferraro <ferraro@users.sourceforge.net>
2007-09-24 16:15:05 +0000

    Created
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/branches/2.0@1854 882a69a1-a10d-0410-8702-ae3607fe02de

---
+++ src/net/sf/hajdbc/distributable/AbstractMembershipListener.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2004-2007, Identity Theft 911, LLC.  All rights reserved.
+ */
+package net.sf.hajdbc.distributable;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.jgroups.Address;
+import org.jgroups.Channel;
+import org.jgroups.MembershipListener;
+import org.jgroups.View;
+
+/**
+ * @author Paul Ferraro
+ */
+public abstract class AbstractMembershipListener implements MembershipListener
+{
+	protected Set<Address> addressSet = new HashSet<Address>();
+	protected Channel channel;
+
+	public AbstractMembershipListener(Channel channel)
+	{
+		this.channel = channel;
+		
+		this.channel.setOpt(Channel.LOCAL, false);
+	}
+	
+	/**
+	 * @see org.jgroups.MembershipListener#block()
+	 */
+	@Override
+	public final void block()
+	{
+	}
+
+	/**
+	 * @see org.jgroups.MembershipListener#suspect(org.jgroups.Address)
+	 */
+	@Override
+	public final void suspect(Address address)
+	{
+	}
+
+	/**
+	 * @see org.jgroups.MembershipListener#viewAccepted(org.jgroups.View)
+	 */
+	@Override
+	public final void viewAccepted(View view)
+	{
+		synchronized (this.addressSet)
+		{
+			Iterator<Address> addresses = this.addressSet.iterator();
+			
+			while (addresses.hasNext())
+			{
+				Address address = addresses.next();
+				
+				if (!view.containsMember(address))
+				{
+					addresses.remove();
+					
+					this.memberLeft(address);
+				}
+			}
+			
+			Address localAddress = this.channel.getLocalAddress();
+			
+			for (Address address: view.getMembers())
+			{
+				// Track remote members only
+				if (!address.equals(localAddress) && this.addressSet.add(address))
+				{
+					this.memberJoined(address);
+				}
+			}
+		}
+	}
+
+	protected abstract void memberJoined(Address address);
+	
+	protected abstract void memberLeft(Address address);
+}
