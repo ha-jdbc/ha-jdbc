d01a409efeaf60e9211907a99b0966593881f927
pferraro <ferraro@users.sourceforge.net>
2008-05-07 18:10:44 +0000

    Fixed bug in quote(String) when database stores upper case characters.
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/branches/2.0@2035 882a69a1-a10d-0410-8702-ae3607fe02de

--- src/net/sf/hajdbc/cache/DatabaseMetaDataSupport.java
+++ src/net/sf/hajdbc/cache/DatabaseMetaDataSupport.java
@@ -24,7 +24,6 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -82,7 +81,7 @@ public class DatabaseMetaDataSupport
 	private static final Pattern LOWER_CASE_PATTERN = Pattern.compile("[a-z]");
 	
 	private Dialect dialect;
-	private Set<String> reservedIdentifierSet;
+	private Set<String> reservedIdentifierSet = new HashSet<String>();
 	private Pattern identifierPattern;
 	private String quote;
 	private boolean supportsMixedCaseIdentifiers;
@@ -91,8 +90,6 @@ public class DatabaseMetaDataSupport
 	private boolean storesLowerCaseQuotedIdentifiers;
 	private boolean storesUpperCaseIdentifiers;
 	private boolean storesUpperCaseQuotedIdentifiers;
-	private boolean storesMixedCaseIdentifiers;
-	private boolean storesMixedCaseQuotedIdentifiers;
 	private boolean supportsSchemasInDDL;
 	private boolean supportsSchemasInDML;
 	
@@ -106,9 +103,6 @@ public class DatabaseMetaDataSupport
 	{
 		this.dialect = dialect;
 		
-		this.reservedIdentifierSet = new HashSet<String>(Arrays.asList(SQL_92_RESERVED_WORDS));
-		this.reservedIdentifierSet.addAll(Arrays.asList(metaData.getSQLKeywords().split(Strings.COMMA)));
-		
 		this.identifierPattern = dialect.getIdentifierPattern(metaData);
 		this.quote = metaData.getIdentifierQuoteString();
 		this.supportsMixedCaseIdentifiers = metaData.supportsMixedCaseIdentifiers();
@@ -117,10 +111,18 @@ public class DatabaseMetaDataSupport
 		this.storesLowerCaseQuotedIdentifiers = metaData.storesLowerCaseQuotedIdentifiers();
 		this.storesUpperCaseIdentifiers = metaData.storesUpperCaseIdentifiers();
 		this.storesUpperCaseQuotedIdentifiers = metaData.storesUpperCaseQuotedIdentifiers();
-		this.storesMixedCaseIdentifiers = metaData.storesMixedCaseIdentifiers();
-		this.storesMixedCaseQuotedIdentifiers = metaData.storesMixedCaseQuotedIdentifiers();
 		this.supportsSchemasInDML = metaData.supportsSchemasInDataManipulation();
 		this.supportsSchemasInDDL = metaData.supportsSchemasInTableDefinitions();
+		
+		for (String word: SQL_92_RESERVED_WORDS)
+		{
+			this.reservedIdentifierSet.add(this.normalizeCase(word));
+		}
+		
+		for (String word: metaData.getSQLKeywords().split(Strings.COMMA))
+		{
+			this.reservedIdentifierSet.add(this.normalizeCase(word));
+		}
 	}
 	
 	/**
@@ -362,29 +364,41 @@ public class DatabaseMetaDataSupport
 	{
 		if (identifier == null) return null;
 		
-		// Driver may return identifiers already quoted.  If so, exit early.
-		if (identifier.startsWith(this.quote)) return this.normalizeMixedCaseQuoted(identifier);
+		int quoteLength = this.quote.length();
+		
+		// Strip any existing quoting
+		String raw = (identifier.startsWith(this.quote) && identifier.endsWith(this.quote)) ? identifier.substring(quoteLength, identifier.length() - quoteLength) : identifier;
+		
+		String normal = this.normalizeCase(raw);
 		
 		// Quote reserved identifiers
-		boolean requiresQuoting = this.reservedIdentifierSet.contains(identifier.toLowerCase());
+		boolean requiresQuoting = this.reservedIdentifierSet.contains(normal);
 		
 		// Quote identifiers containing special characters
-		requiresQuoting |= !this.identifierPattern.matcher(identifier).matches();
+		requiresQuoting |= !this.identifierPattern.matcher(raw).matches();
 		
 		// Quote mixed-case identifiers if detected and supported by DBMS
-		requiresQuoting |= !this.supportsMixedCaseIdentifiers && this.supportsMixedCaseQuotedIdentifiers && ((this.storesLowerCaseIdentifiers && UPPER_CASE_PATTERN.matcher(identifier).find()) || (this.storesUpperCaseIdentifiers && LOWER_CASE_PATTERN.matcher(identifier).find()));
+		requiresQuoting |= !this.supportsMixedCaseIdentifiers && this.supportsMixedCaseQuotedIdentifiers && ((this.storesLowerCaseIdentifiers && !this.storesLowerCaseQuotedIdentifiers && UPPER_CASE_PATTERN.matcher(raw).find()) || (this.storesUpperCaseIdentifiers && !this.storesUpperCaseQuotedIdentifiers && LOWER_CASE_PATTERN.matcher(raw).find()));
 		
-		return requiresQuoting ? this.quote + this.normalizeMixedCaseQuoted(identifier) + this.quote : this.normalizeMixedCase(identifier);
+		return requiresQuoting ? this.quote + this.normalizeCaseQuoted(raw) + this.quote : normal;
 	}
 	
-	private String normalizeMixedCase(String identifier)
+	private String normalizeCase(String identifier)
 	{
-		return this.storesMixedCaseIdentifiers ? identifier.toLowerCase() : identifier;
+		if (this.storesLowerCaseIdentifiers) return identifier.toLowerCase();
+		
+		if (this.storesUpperCaseIdentifiers) return identifier.toUpperCase();
+		
+		return identifier;
 	}
 	
-	private String normalizeMixedCaseQuoted(String identifier)
+	private String normalizeCaseQuoted(String identifier)
 	{
-		return this.storesMixedCaseQuotedIdentifiers ? identifier.toLowerCase() : identifier;
+		if (this.storesLowerCaseQuotedIdentifiers) return identifier.toLowerCase();
+		
+		if (this.storesUpperCaseQuotedIdentifiers) return identifier.toUpperCase();
+		
+		return identifier;
 	}
 	
 	private String normalize(String qualifiedName, String defaultSchema)
@@ -393,17 +407,6 @@ public class DatabaseMetaDataSupport
 
 		String name = parts[parts.length - 1];
 		String schema = (parts.length > 1) ? parts[parts.length - 2] : defaultSchema;
-
-		boolean quoted = name.startsWith(this.quote);
-		
-		if ((!quoted && this.storesLowerCaseIdentifiers) || (quoted && this.storesLowerCaseQuotedIdentifiers))
-		{
-			name = name.toLowerCase();
-		}
-		else if ((!quoted && this.storesUpperCaseIdentifiers) || (quoted && this.storesUpperCaseQuotedIdentifiers))
-		{
-			name = name.toUpperCase();
-		}
 			
 		return this.qualifyNameForDML(new QualifiedName(schema, name));
 	}
