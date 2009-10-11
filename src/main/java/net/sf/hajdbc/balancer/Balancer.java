7cedce8818d5356f0fafd4a34560d9f6106af291
pferraro <ferraro@users.sourceforge.net>
2009-10-11 04:01:33 +0000

    Fixed copyright headers
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@2224 882a69a1-a10d-0410-8702-ae3607fe02de

--- src/main/java/net/sf/hajdbc/balancer/Balancer.java
+++ src/main/java/net/sf/hajdbc/balancer/Balancer.java
@@ -1,17 +1,17 @@
 /*
- * HA-JDBC: High-Availablity JDBC
- * Copyright 2004, 2009 Paul Ferraro
- *
+ * HA-JDBC: High-Availability JDBC
+ * Copyright 2004-2009 Paul Ferraro
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,15 +22,12 @@ import java.util.Set;
 import net.sf.hajdbc.Database;
 
 /**
- * @author  Paul Ferraro
- * @since   1.0
- * @param <D> either java.sql.Driver or javax.sql.DataSource
- */
-/**
- * @author paul
+ * The set of active databases and a strategy for determining the next balancing target.
+ * 
+ * @author Paul Ferraro
  *
- * @param <Z>
- * @param <D>
+ * @param <Z> the database connection source
+ * @param <D> the database descriptor
  */
 public interface Balancer<Z, D extends Database<Z>> extends Set<D>
 {
