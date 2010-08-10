889bf9ad50214e64dfaca1c3b145840c0674dfe1
pferraro <ferraro@users.sourceforge.net>
2010-08-10 02:05:40 +0000

    Created
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@2369 882a69a1-a10d-0410-8702-ae3607fe02de

---
+++ src/test/java/net/sf/hajdbc/MockDatabase.java
@@ -0,0 +1,70 @@
+/*
+ * HA-JDBC: High-Availability JDBC
+ * Copyright (c) 2004-2007 Paul Ferraro
+ * 
+ * This library is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by the 
+ * Free Software Foundation; either version 2.1 of the License, or (at your 
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ * 
+ * Contact: ferraro@users.sourceforge.net
+ */
+package net.sf.hajdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import net.sf.hajdbc.sql.AbstractDatabase;
+
+/**
+ * @author Paul Ferraro
+ *
+ */
+public class MockDatabase extends AbstractDatabase<Void>
+{
+	public MockDatabase()
+	{
+		this("");
+	}
+	
+	public MockDatabase(String id)
+	{
+		this(id, 1);
+	}
+	
+	public MockDatabase(String id, int weight)
+	{
+		this.setId(id);
+		this.setWeight(weight);
+		this.clean();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.hajdbc.Database#connect(java.lang.Object, java.lang.String)
+	 */
+	@Override
+	public Connection connect(Void connectionSource, String password) throws SQLException
+	{
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.hajdbc.Database#createConnectionSource()
+	 */
+	@Override
+	public Void createConnectionSource()
+	{
+		return null;
+	}
+}
