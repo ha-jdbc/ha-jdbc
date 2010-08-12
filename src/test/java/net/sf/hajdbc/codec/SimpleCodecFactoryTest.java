2688509d21581f6a6e41f50a6fdb85547190d7a9
pferraro <ferraro@users.sourceforge.net>
2010-08-12 04:16:53 +0000

    Codec tests.
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@2379 882a69a1-a10d-0410-8702-ae3607fe02de

---
+++ src/test/java/net/sf/hajdbc/codec/SimpleCodecFactoryTest.java
@@ -0,0 +1,50 @@
+/*
+ * HA-JDBC: High-Availablity JDBC
+ * Copyright 2010 Paul Ferraro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.hajdbc.codec;
+
+import java.sql.SQLException;
+
+import junit.framework.Assert;
+
+import net.sf.hajdbc.codec.simple.SimpleCodecFactory;
+
+import org.junit.Test;
+
+/**
+ * @author Paul Ferraro
+ */
+public class SimpleCodecFactoryTest
+{
+	private CodecFactory factory = new SimpleCodecFactory();
+	
+	@Test
+	public void encode() throws SQLException
+	{
+		Codec codec = this.factory.createCodec(null);
+		
+		Assert.assertEquals("password", codec.encode("password"));
+	}
+	
+	@Test
+	public void decode() throws SQLException
+	{
+		Codec codec = this.factory.createCodec(null);
+		
+		Assert.assertEquals("password", codec.decode("password"));
+	}
+}
