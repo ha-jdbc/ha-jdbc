0b296bdd966861e3540b674f08cb5264afc21371
pferraro <ferraro@users.sourceforge.net>
2010-12-22 14:53:53 +0000

    Add jboss-logging provider.
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@2436 882a69a1-a10d-0410-8702-ae3607fe02de

--- src/main/java/net/sf/hajdbc/logging/jboss/JBossLogger.java
+++ src/main/java/net/sf/hajdbc/logging/jboss/JBossLogger.java
@@ -1,51 +1,52 @@
 /*
- * HA-JDBC: High-Availability JDBC
- * Copyright 2004-2009 Paul Ferraro
- * 
+ * HA-JDBC: High-Availablity JDBC
+ * Copyright 2010 Paul Ferraro
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package net.sf.hajdbc.logging.jdk;
+package net.sf.hajdbc.logging.jboss;
 
 import java.util.EnumMap;
 import java.util.Map;
+
+import org.jboss.logging.Logger;
 
 import net.sf.hajdbc.logging.AbstractLogger;
 import net.sf.hajdbc.logging.Level;
 
 /**
- * java.util.logging-based {@link net.sf.hajdbc.logging.Logger} implementation.
  * @author Paul Ferraro
  */
-public class JDKLogger extends AbstractLogger
+public class JBossLogger extends AbstractLogger
 {
-	private static final Map<Level, java.util.logging.Level> levels = new EnumMap<Level, java.util.logging.Level>(Level.class);
+	private static final Map<Level, org.jboss.logging.Logger.Level> levels = new EnumMap<Level, org.jboss.logging.Logger.Level>(Level.class);
 	static
 	{
-		levels.put(Level.ERROR, java.util.logging.Level.SEVERE);
-		levels.put(Level.WARN, java.util.logging.Level.WARNING);
-		levels.put(Level.INFO, java.util.logging.Level.INFO);
-		levels.put(Level.DEBUG, java.util.logging.Level.FINE);
-		levels.put(Level.TRACE, java.util.logging.Level.FINEST);
+		levels.put(Level.ERROR, org.jboss.logging.Logger.Level.ERROR);
+		levels.put(Level.WARN, org.jboss.logging.Logger.Level.WARN);
+		levels.put(Level.INFO, org.jboss.logging.Logger.Level.INFO);
+		levels.put(Level.DEBUG, org.jboss.logging.Logger.Level.DEBUG);
+		levels.put(Level.TRACE, org.jboss.logging.Logger.Level.TRACE);
 	}
 	
-	private final java.util.logging.Logger logger;
+	private final Logger logger;
 	
-	public JDKLogger(Class<?> targetClass)
+	public JBossLogger(Class<?> targetClass)
 	{
-		this.logger = java.util.logging.Logger.getLogger(targetClass.getName());
+		this.logger = Logger.getLogger(targetClass);
 	}
-
+	
 	/**
 	 * {@inheritDoc}
 	 * @see net.sf.hajdbc.logging.Logger#log(net.sf.hajdbc.logging.Level, java.lang.Throwable, java.lang.String, java.lang.Object[])
@@ -53,20 +54,11 @@ public class JDKLogger extends AbstractLogger
 	@Override
 	public void log(Level level, Throwable e, String pattern, Object... args)
 	{
-		java.util.logging.Level realLevel = levels.get(level);
+		org.jboss.logging.Logger.Level realLevel = levels.get(level);
 		
-		if (this.logger.isLoggable(realLevel))
+		if (this.logger.isEnabled(realLevel))
 		{
-			String message = format(pattern, args);
-			
-			if (e != null)
-			{
-				this.logger.log(realLevel, message, e);
-			}
-			else
-			{
-				this.logger.log(realLevel, message);
-			}
+			this.logger.log(realLevel, format(pattern, args), e);
 		}
 	}
 }
---
+++ src/main/java/net/sf/hajdbc/logging/jboss/JBossLoggingProvider.java
@@ -0,0 +1,58 @@
+/*
+ * HA-JDBC: High-Availablity JDBC
+ * Copyright 2010 Paul Ferraro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sf.hajdbc.logging.jboss;
+
+import net.sf.hajdbc.logging.Logger;
+import net.sf.hajdbc.logging.LoggingProvider;
+
+/**
+ * @author Paul Ferraro
+ */
+public class JBossLoggingProvider implements LoggingProvider
+{
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.hajdbc.logging.LoggingProvider#isEnabled()
+	 */
+	@Override
+	public boolean isEnabled()
+	{
+		// If this class could be loaded, so can jboss logging.
+		return true;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.hajdbc.logging.LoggingProvider#getLogger(java.lang.Class)
+	 */
+	@Override
+	public Logger getLogger(Class<?> targetClass)
+	{
+		return new JBossLogger(targetClass);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see net.sf.hajdbc.logging.LoggingProvider#getName()
+	 */
+	@Override
+	public String getName()
+	{
+		return "JBoss";
+	}
+}
