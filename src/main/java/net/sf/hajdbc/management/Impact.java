1337fd5ab56d90a4b297e8de452de86ff94eb5f0
pferraro <ferraro@users.sourceforge.net>
2011-01-27 15:12:20 +0000

    Add impact.
    git-svn-id: https://ha-jdbc.svn.sourceforge.net/svnroot/ha-jdbc/trunk@2473 882a69a1-a10d-0410-8702-ae3607fe02de

--- src/main/java/net/sf/hajdbc/management/Impact.java
+++ src/main/java/net/sf/hajdbc/management/Impact.java
@@ -1,37 +1,53 @@
 /*
- * HA-JDBC: High-Availability JDBC
- * Copyright 2010 Paul Ferraro
- * 
+ * HA-JDBC: High-Availablity JDBC
+ * Copyright 2011 Paul Ferraro
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.sf.hajdbc.management;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import javax.management.MBeanOperationInfo;
 
 /**
- * Placeholder for JSR 255 (JMX 2.0) annotations.
- * 
  * @author Paul Ferraro
  */
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-@Documented
-public @interface ManagedOperation
+public enum Impact
 {
-	Impact impact() default Impact.UNKNOWN;
+	ACTION(MBeanOperationInfo.ACTION),
+	ACTION_INFO(MBeanOperationInfo.ACTION_INFO),
+	INFO(MBeanOperationInfo.INFO),
+	UNKNOWN(MBeanOperationInfo.UNKNOWN);
+	
+	private final int code;
+	
+	private Impact(int code)
+	{
+		this.code = code;
+	}
+	
+	public static Impact forCode(int code)
+	{
+		for (Impact impact: Impact.values())
+		{
+			if (impact.code == code) return impact;
+		}
+		
+		throw new IllegalArgumentException(code + " is not a valid mbean operation impact code");
+	}
+	
+	public int getCode()
+	{
+		return this.code;
+	}
 }
