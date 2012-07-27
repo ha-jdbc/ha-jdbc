593e60b3a532a5b77cb9f15d5cf0342371bbb1da
Paul Ferraro <paul.ferraro@redhat.com>
2012-07-27 10:18:54 -0400

    Parameterize trace logging.

--- src/main/java/net/sf/hajdbc/sql/AbstractInvocationHandler.java
+++ src/main/java/net/sf/hajdbc/sql/AbstractInvocationHandler.java
@@ -105,7 +105,7 @@ public abstract class AbstractInvocationHandler<Z, D extends Database<Z>, T, E e
 
 		Invoker<Z, D, T, R, E> invoker = this.getInvoker(object, method, parameters);
 		
-		this.logger.log(Level.TRACE, "Invoking " + method.getName() + " using " + strategy.getClass().getName());
+		this.logger.log(Level.TRACE, "Invoking {0} using {1}", method, strategy.getClass().getName());
 		
 		SortedMap<D, R> results = strategy.invoke(this, invoker);
 		
