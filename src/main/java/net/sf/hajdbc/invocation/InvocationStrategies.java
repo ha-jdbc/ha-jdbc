37a1f681a137868bb6ef8eb615ddfd47ad67ede4
Paul Ferraro <paul.ferraro@redhat.com>
2013-03-22 10:48:23 -0400

    Add trace logging of invocation strategy.

--- src/main/java/net/sf/hajdbc/invocation/InvocationStrategies.java
+++ src/main/java/net/sf/hajdbc/invocation/InvocationStrategies.java
@@ -3,6 +3,9 @@ package net.sf.hajdbc.invocation;
 import java.util.SortedMap;
 
 import net.sf.hajdbc.Database;
+import net.sf.hajdbc.logging.Level;
+import net.sf.hajdbc.logging.Logger;
+import net.sf.hajdbc.logging.LoggerFactory;
 import net.sf.hajdbc.sql.ProxyFactory;
 
 public enum InvocationStrategies implements InvocationStrategy
@@ -16,6 +19,8 @@ public enum InvocationStrategies implements InvocationStrategy
 	END_TRANSACTION_INVOKE_ON_ALL(new InvokeOnManyInvocationStrategy(new AllResultsCollector(new TransactionalExecutorProvider(true)))),
 	;
 	
+	private static final Logger logger = LoggerFactory.getLogger(SimpleInvoker.class);
+	
 	private final InvocationStrategy strategy;
 	
 	private InvocationStrategies(InvocationStrategy strategy)
@@ -26,6 +31,7 @@ public enum InvocationStrategies implements InvocationStrategy
 	@Override
 	public <Z, D extends Database<Z>, T, R, E extends Exception> SortedMap<D, R> invoke(ProxyFactory<Z, D, T, E> map, Invoker<Z, D, T, R, E> invoker) throws E
 	{
+		logger.log(Level.TRACE, "Invoking {0} using {1} strategy.", invoker, this);
 		return this.strategy.invoke(map, invoker);
 	}
 }
