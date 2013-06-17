679a64f45536bc9ddaa565f46d9b27618a77e01d
Paul Ferraro <paul.ferraro@redhat.com>
2013-06-17 12:30:55 -0400

    Add test for DataSource-based config parsing.

--- src/test/java/net/sf/hajdbc/xml/XMLDatabaseClusterConfigurationFactoryTest.java
+++ src/test/java/net/sf/hajdbc/xml/XMLDatabaseClusterConfigurationFactoryTest.java
@@ -29,20 +29,19 @@ import static org.mockito.Mockito.when;
 
 import java.io.StringReader;
 import java.io.StringWriter;
-import java.sql.Driver;
 import java.sql.SQLException;
 import java.util.Map;
 
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import net.sf.hajdbc.Database;
 import net.sf.hajdbc.DatabaseClusterConfiguration;
 import net.sf.hajdbc.SynchronizationStrategy;
 import net.sf.hajdbc.balancer.load.LoadBalancerFactory;
 import net.sf.hajdbc.cache.eager.EagerDatabaseMetaDataCacheFactory;
 import net.sf.hajdbc.dialect.StandardDialectFactory;
 import net.sf.hajdbc.durability.coarse.CoarseDurabilityFactory;
-import net.sf.hajdbc.sql.DriverDatabase;
 import net.sf.hajdbc.sql.DriverDatabaseClusterConfiguration;
 import net.sf.hajdbc.sql.TransactionModeEnum;
 import net.sf.hajdbc.state.StateManagerFactory;
@@ -54,7 +53,18 @@ import org.junit.Test;
 public class XMLDatabaseClusterConfigurationFactoryTest
 {
 	@Test
-	public void createConfiguration() throws SQLException
+	public void createDriverBasedConfiguration() throws SQLException
+	{
+		this.createConfiguration(DriverDatabaseClusterConfiguration.class, "jdbc:mock:db1", "jdbc:mock:db2");
+	}
+	
+	@Test
+	public void createDataSourceBasedConfiguration() throws SQLException
+	{
+		this.createConfiguration(DriverDatabaseClusterConfiguration.class, "java:comp/env/jdbc/db1", "java:comp/env/jdbc/db2");
+	}
+	
+	private <Z, D extends Database<Z>, C extends DatabaseClusterConfiguration<Z, D>> void createConfiguration(Class<C> configClass, String location1, String location2) throws SQLException
 	{
 		StringBuilder builder = new StringBuilder();
 		builder.append("<?xml version=\"1.0\"?>");
@@ -62,8 +72,8 @@ public class XMLDatabaseClusterConfigurationFactoryTest
 		builder.append("\t<sync id=\"diff\"><property name=\"fetchSize\">100</property><property name=\"maxBatchSize\">100</property></sync>");
 		builder.append("\t<state id=\"sql\"><property name=\"urlPattern\">jdbc:h2:{0}</property><property name=\"minIdle\">1</property></state>");
 		builder.append("\t<cluster default-sync=\"diff\">");
-		builder.append("\t\t<database id=\"db1\" location=\"jdbc:mock:db1\"/>");
-		builder.append("\t\t<database id=\"db2\" location=\"jdbc:mock:db2\"/>");
+		builder.append(String.format("\t\t<database id=\"db1\" location=\"%s\"/>", location1));
+		builder.append(String.format("\t\t<database id=\"db2\" location=\"%s\"/>", location2));
 		builder.append("\t</cluster>");
 		builder.append("</ha-jdbc>");
 		
@@ -71,11 +81,11 @@ public class XMLDatabaseClusterConfigurationFactoryTest
 		
 		XMLStreamFactory streamFactory = mock(XMLStreamFactory.class);
 		
-		XMLDatabaseClusterConfigurationFactory<Driver, DriverDatabase> factory = new XMLDatabaseClusterConfigurationFactory<Driver, DriverDatabase>(DriverDatabaseClusterConfiguration.class, streamFactory);
+		XMLDatabaseClusterConfigurationFactory<Z, D> factory = new XMLDatabaseClusterConfigurationFactory<Z, D>(configClass, streamFactory);
 		
 		when(streamFactory.createSource()).thenReturn(new StreamSource(new StringReader(xml)));
 		
-		DatabaseClusterConfiguration<Driver, DriverDatabase> configuration = factory.createConfiguration();
+		DatabaseClusterConfiguration<Z, D> configuration = factory.createConfiguration();
 		
 		assertNull(configuration.getDispatcherFactory());
 		Map<String, SynchronizationStrategy> syncStrategies = configuration.getSynchronizationStrategyMap();
@@ -117,26 +127,26 @@ public class XMLDatabaseClusterConfigurationFactoryTest
 		assertFalse(configuration.isRandEvaluationEnabled());
 		assertFalse(configuration.isSequenceDetectionEnabled());
 		
-		Map<String, DriverDatabase> databases = configuration.getDatabaseMap();
+		Map<String, D> databases = configuration.getDatabaseMap();
 		
 		assertNotNull(databases);
 		assertEquals(2, databases.size());
 		
-		DriverDatabase db1 = databases.get("db1");
+		D db1 = databases.get("db1");
 		
 		assertNotNull(db1);
 		assertEquals("db1", db1.getId());
-		assertEquals("jdbc:mock:db1", db1.getLocation());
+		assertEquals(location1, db1.getLocation());
 		assertEquals(1, db1.getWeight());
 		assertFalse(db1.isLocal());
 		assertFalse(db1.isActive());
 		assertFalse(db1.isDirty());
 		
-		DriverDatabase db2 = databases.get("db2");
+		D db2 = databases.get("db2");
 		
 		assertNotNull(db2);
 		assertEquals("db2", db2.getId());
-		assertEquals("jdbc:mock:db2", db2.getLocation());
+		assertEquals(location2, db2.getLocation());
 		assertEquals(1, db2.getWeight());
 		assertFalse(db2.isLocal());
 		assertFalse(db2.isActive());
