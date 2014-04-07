ad86182d62d0b721c2c402d8ddd33ee298dee346
Paul Ferraro <paul.ferraro@redhat.com>
2014-04-07 13:40:25 -0400

    Add missing license headers.

--- core/src/main/java/net/sf/hajdbc/configuration/ServiceBuilder.java
+++ core/src/main/java/net/sf/hajdbc/configuration/ServiceBuilder.java
@@ -1,3 +1,20 @@
+/*
+ * HA-JDBC: High-Availability JDBC
+ * Copyright (C) 2014  Paul Ferraro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.hajdbc.configuration;
 
 import java.beans.IntrospectionException;
@@ -30,7 +47,7 @@ public class ServiceBuilder<T extends Identifiable> extends SimpleServiceBuilder
 	}
 	
 	@Override
-	public SimpleServiceBuilder<T> read(T service)
+	public ServiceBuilder<T> read(T service)
 	{
 		super.read(service);
 		for (Map.Entry<String, Map.Entry<PropertyDescriptor, PropertyEditor>> entry: findDescriptors(service.getClass()).entrySet())
--- core/src/main/java/net/sf/hajdbc/xml/Namespace.java
+++ core/src/main/java/net/sf/hajdbc/xml/Namespace.java
@@ -1,3 +1,20 @@
+/*
+ * HA-JDBC: High-Availability JDBC
+ * Copyright (C) 2014  Paul Ferraro
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.sf.hajdbc.xml;
 
 import java.net.URL;
@@ -11,7 +28,7 @@ import org.xml.sax.SAXException;
 public enum Namespace
 {
 	VERSION_3_0(3, 0, DatabaseClusterConfigurationReader_3_0.FACTORY),
-//	VERSION_3_1(3, 1, DatabaseClusterConfigurationReader_3_1.FACTORY),
+	VERSION_3_1(3, 1, DatabaseClusterConfigurationReader_3_1.FACTORY),
 	;
 
 	public static final Namespace CURRENT_VERSION = VERSION_3_0;
